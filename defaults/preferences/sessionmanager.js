3d1fd85b2d9623e6a5a9336dad222ab2208c384d
Michael Kraft <morac99-firefox2@yahoo.com>
2007-08-08 07:22:17 +0000

    accidently changed the wrong preference value

--- defaults/preferences/sessionmanager.js
+++ defaults/preferences/sessionmanager.js
@@ -10,9 +10,9 @@ pref("browser.sessionstore.resume_from_crash", true);
 pref("browser.sessionstore.resume_session_once", false);
 
 pref("extensions.sessionmanager.autosave_session", true);
-pref("extensions.sessionmanager.backup_session", 2);
+pref("extensions.sessionmanager.backup_session", 1);
 pref("extensions.sessionmanager.disable_cache_fixer", false);
-pref("extensions.sessionmanager.max_backup_keep", 1);
+pref("extensions.sessionmanager.max_backup_keep", 2);
 pref("extensions.sessionmanager.max_closed_undo", 10);
 pref("extensions.sessionmanager.name_format", "%40t-%d");
 pref("extensions.sessionmanager.options_selected_tab", 0);
