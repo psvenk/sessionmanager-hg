189c7aadf35c8902f219564856bc7138b2d707de
Michael Kraft <morac99-firefox2@yahoo.com>
2008-08-22 14:01:53 +0000

    Add options for: - middle click restores tab - hiding Tools menu Session Manager menu - Maximum Sessions to display - saving session cookies in sesssions
    Change max backups to 10

--- defaults/preferences/sessionmanager.js
+++ defaults/preferences/sessionmanager.js
@@ -4,16 +4,22 @@ pref("browser.sessionstore.restore_prompt_uri", "chrome://sessionmanager/content
 
 pref("extensions.sessionmanager.autosave_session", true);
 pref("extensions.sessionmanager.backup_session", 1);
+pref("extensions.sessionmanager.click_restore_tab", true);
 pref("extensions.sessionmanager.disable_cache_fixer", false);
 pref("extensions.sessionmanager.encrypt_sessions", false);
-pref("extensions.sessionmanager.max_backup_keep", 2);
+pref("extensions.sessionmanager.hide_tools_menu", false);
+pref("extensions.sessionmanager.max_backup_keep", 10);
 pref("extensions.sessionmanager.max_closed_undo", 10);
+pref("extensions.sessionmanager.max_display", 20);
 pref("extensions.sessionmanager.name_format", "%40t-%d");
 pref("extensions.sessionmanager.options_selected_tab", 0);
 pref("extensions.sessionmanager.overwrite", true);
 pref("extensions.sessionmanager.reload", false);
 pref("extensions.sessionmanager.resume_session", "");
 pref("extensions.sessionmanager.save_closed_tabs", 1);
+pref("extensions.sessionmanager.save_cookies", true);
 pref("extensions.sessionmanager.save_window_list", false);
+pref("extensions.sessionmanager.sessions_dir", "");
 pref("extensions.sessionmanager.session_list_order", 1);
+pref("extensions.sessionmanager.startup", 0);
 pref("extensions.sessionmanager.submenus", false);
