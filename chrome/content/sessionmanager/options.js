19dd72647c0433b0a7e57ee5bebd4e5126d8e487
Michael Kraft <morac99-firefox2@yahoo.com>
2009-05-28 14:02:29 +0000

    1. Use new comparison class to check for SeaMonkey.

--- chrome/content/sessionmanager/options.js
+++ chrome/content/sessionmanager/options.js
@@ -36,7 +36,7 @@ gSessionManager.onLoad = function() {
 	startupSelect(_("startupOption").selectedIndex = _("extensions.sessionmanager.startup").valueFromPreferences);
 	
 	// Hide close tab restoration preferences in SeaMonkey since it doesn't work
-	if (this.mAppID == "SEAMONKEY") {
+	if (this.mApp.idEquals("SEAMONKEY")) {
 		_("save_closed_tabs").parentNode.style.visibility = "collapse";
 	}
 	
