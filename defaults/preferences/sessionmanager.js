f0f5e4f11e97f360d4022e988b79f3310acef6a4
Michael Kraft <morac99-firefox2@yahoo.com>
2009-08-13 14:36:27 +0000

    Remove shutdown on last window closed peference - set dynamically now

--- defaults/preferences/sessionmanager.js
+++ defaults/preferences/sessionmanager.js
@@ -31,7 +31,6 @@ pref("extensions.sessionmanager.save_cookies", true);
 pref("extensions.sessionmanager.save_window_list", true);
 pref("extensions.sessionmanager.sessions_dir", "");
 pref("extensions.sessionmanager.session_list_order", 1);
-pref("extensions.sessionmanager.shutdown_on_last_window_close", false);
 pref("extensions.sessionmanager.startup", 0);
 pref("extensions.sessionmanager.submenus", false);
 pref("extensions.sessionmanager.update_message", true);
