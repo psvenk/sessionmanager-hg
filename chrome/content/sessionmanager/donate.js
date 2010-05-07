52e2c919d41e03a64270bc96bb79b358f51160a4
Michael Kraft <morac99-firefox2@yahoo.com>
2010-05-07 21:56:38 +0000

    1. Fixed issue with contribute link not working if Woot Watcher addon installed. 2. Fixed issue with contribute link duplicating with Mr ToolTech addon installed.

--- chrome/content/sessionmanager/donate.js
+++ chrome/content/sessionmanager/donate.js
@@ -1,47 +1,46 @@
+var gSessionManagerDonate = {
+	startup: function() {
+		db_gExtensionsView = document.getElementById("extensionsView");
+		db_gExtensionsView.addEventListener("select", gSessionManagerDonate.doIt, false);
+	},
 
-window.addEventListener("load", startup, true);
+	doIt: function() {
+		var myext = document.getElementById("urn:mozilla:item:{1280606b-2510-4fe0-97ef-9b5a22eafe30}");
+		var db_donateContainer = document.getElementById("session_manager_donateContainer");
+		var donateSpacer = document.getElementById("session_manager_donateSpacer");
+		var nameVersionBox;
+		
+		try {
+			nameVersionBox = document.getAnonymousElementByAttribute(myext, "anonid", "addonNameVersion");
+			if(!nameVersionBox)
+				nameVersionBox = document.getAnonymousElementByAttribute(myext, "class", "addon-name-version");
+			
+			// Only add link if not already there
+			if (!nameVersionBox.getElementsByClassName("session_manager_donate").length) {
+				var spacerClone = donateSpacer.cloneNode(true);
+				spacerClone.hidden = false;
+				nameVersionBox.appendChild(spacerClone);
+				
+				var containerClone = db_donateContainer.cloneNode(true);
+				containerClone.hidden = false;
+				nameVersionBox.appendChild(containerClone);
+			}
+		} catch (e) {}
+	},
 
-function startup() {
-
-db_gExtensionsView = document.getElementById("extensionsView");
-db_gExtensionsView.addEventListener("select", doIt, false);
-
+	link: function(url) {
+		var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService();
+		var wmed = wm.QueryInterface(Components.interfaces.nsIWindowMediator);
+		var win = wmed.getMostRecentWindow("navigator:browser");
+		if (!win) {
+			alert("Cannot open a new tab!");
+		}
+		else {
+			var content = win.document.getElementById("content");
+			content.selectedTab = content.addTab(url);	
+		}
+	}
 }
 
-		
-function doIt() {
+window.addEventListener("load", gSessionManagerDonate.startup, true);
 
-	var myext = document.getElementById("urn:mozilla:item:{1280606b-2510-4fe0-97ef-9b5a22eafe30}");
-	var db_donateContainer = document.getElementById("db_donateContainer");
-	var donateSpacer = document.getElementById("donateSpacer");
-	var nameVersionBox;
-	
-	try {
-		nameVersionBox = document.getAnonymousElementByAttribute(myext, "anonid", "addonNameVersion");
-		if(!nameVersionBox)
-			nameVersionBox = document.getAnonymousElementByAttribute(myext, "class", "addon-name-version");
-		
-		var spacerClone = donateSpacer.cloneNode(true);
-		spacerClone.hidden = false;
-		nameVersionBox.appendChild(spacerClone);
-		
-		var containerClone = db_donateContainer.cloneNode(true);
-		containerClone.hidden = false;
-		nameVersionBox.appendChild(containerClone);
-		
-	} catch (e) {}
-}
-
-
-function link(url) {
-	var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService();
-  var wmed = wm.QueryInterface(Components.interfaces.nsIWindowMediator);
-  var win = wmed.getMostRecentWindow("navigator:browser");
-    if (!win) {
-      alert("Cannot open a new tab!");
-    }
-    else {
-    	var content = win.document.getElementById("content");
-    	content.selectedTab = content.addTab(url);	
-    }
-}
